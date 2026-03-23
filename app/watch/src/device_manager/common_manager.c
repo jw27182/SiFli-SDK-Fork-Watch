31bdd2c428ce4fb43238d35064c0f78a4e88d1b3
jw27182 <1506513124@qq.com>
2026-03-24 00:53:06 +0800

    Feature[watch]:新增device统一初始化位置

--- app/watch/src/device_manager/common_manager.c
+++ app/watch/src/device_manager/common_manager.c
@@ -1,5 +1,27 @@
 #include "st_lsm6dsl_sensor_v1.h"
 #include "sensor_liteon_ltr303.h"
+#include "battery_calculator.h"
+
+battery_calculator_t battery_calc;
+
+static int battery_init(void)
+{
+    static battery_calculator_config_t calc_config = {
+        .charging_table = charging_curve_table,
+        .discharging_table = discharge_curve_table,
+        .charge_filter_threshold = 50,     // 充电时50mV阈值
+        .discharge_filter_threshold = 30,  // 放电时30mV阈值
+        .filter_count = 3,                 // 需要3次确认
+        .secondary_filter_enabled = true,  // 启用二级滤波
+        .secondary_filter_weight_pre = 90, // 前电压权重90%
+        .secondary_filter_weight_cur = 10  // 当前电压权重10%
+    };
+
+    calc_config.charging_table_size = charging_curve_table_size;
+    calc_config.discharging_table_size = discharge_curve_table_size;
+    
+    return battery_calculator_init(&battery_calc, &calc_config);
+}
 
 static int lsm6dsl_init(void)
 {
@@ -10,8 +32,6 @@ static int lsm6dsl_init(void)
     return rt_hw_lsm6dsl_init("lsm6dsl", &cfg);
 }
 
-INIT_DEVICE_EXPORT(lsm6dsl_init);
-
 static int ltr303_init(void)
 {
     struct rt_sensor_config cfg = {
@@ -20,4 +40,6 @@ static int ltr303_init(void)
     return rt_hw_ltr303_init("ltr303", &cfg);
 }
 
+INIT_DEVICE_EXPORT(battery_init);
+INIT_DEVICE_EXPORT(lsm6dsl_init);
 INIT_DEVICE_EXPORT(ltr303_init);
